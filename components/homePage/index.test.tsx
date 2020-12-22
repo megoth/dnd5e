983ec24213f695a37edfb0778afd9b4f378f0935
Arne Hassel <arne.hassel@gmail.com>
2020-12-22 09:58:21 +0100

    Making sure that no state change happens in homePage tests

--- components/homePage/index.test.tsx
+++ components/homePage/index.test.tsx
@@ -3,13 +3,19 @@ import { render } from "@testing-library/react";
 import useApp from "../../src/hooks/useApp";
 import mockAppHook from "../../__testUtils/mockAppHook";
 import HomePage from "./index";
+import useDataset from "../../src/hooks/useDataset";
+import { mockProfileDataset } from "../../__testUtils/mockProfileDataset";
 
 jest.mock("../../src/hooks/useApp");
 const mockedAppHook = useApp as jest.Mock;
 
+jest.mock("../../src/hooks/useDataset");
+const mockedUseDataset = useDataset as jest.Mock;
+
 describe("HomePage", () => {
   it("renders", () => {
     mockAppHook(mockedAppHook);
+    mockedUseDataset.mockReturnValue(mockProfileDataset());
     const { asFragment } = render(<HomePage />);
     expect(asFragment()).toMatchSnapshot();
   });
