b18db1beeb32e28c53267d4d99cba9bab27b2bdf
Arne Hassel <arne.hassel@gmail.com>
2020-12-27 17:32:58 +0100

    Implemented PWA

--- next.config.js
+++ next.config.js
@@ -1,22 +1,30 @@
+const withPWA = require("next-pwa");
 const withSourceMaps = require("@zeit/next-source-maps");
 const path = require("path");
 
-module.exports = withSourceMaps({
-  webpack(config) {
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: config.module.rules.concat([
-          {
-            test: /\.(md|ttl)$/i,
-            use: "raw-loader",
-          },
-        ]),
-      },
-    };
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-});
+module.exports = withPWA(
+  withSourceMaps({
+    pwa: {
+      disable: process.env.NODE_ENV === "development",
+      register: true,
+      dest: "public",
+    },
+    webpack(config) {
+      return {
+        ...config,
+        module: {
+          ...config.module,
+          rules: config.module.rules.concat([
+            {
+              test: /\.(md|ttl)$/i,
+              use: "raw-loader",
+            },
+          ]),
+        },
+      };
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+    },
+  })
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.scss";
 import { SessionProvider } from "@inrupt/solid-ui-react";
-import AppConfig from "../components/appConfig";
+import Head from "next/head";
 import { ensureAbsoluteURL, prefersDarkModeScheme } from "../src/windowHelpers";
+import AppConfig from "../components/appConfig";
 
 const APP_INDEX_URL = ensureAbsoluteURL(
   process.env.NEXT_PUBLIC_APP_INDEX_URL || ""
@@ -19,10 +20,84 @@ export default function App({ Component, pageProps }: AppProps) {
       .classList.toggle("dark", prefersDarkModeScheme());
   });
   return (
-    <SessionProvider sessionId="dnd5e-app">
-      <AppConfig appIndexURL={APP_INDEX_URL} appVocabURL={APP_VOCAB_URL}>
-        <Component {...pageProps} />
-      </AppConfig>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>D&D5e Solid App</title>
+        <meta name="application-name" content="D&D5e Solid App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="&D5e Solid App" />
+        <meta
+          name="description"
+          content="Solid App for Dungeons & Dragons 5th edition"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
+        />
+        <meta name="msapplication-TileColor" content="#DC2626" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#DC2626" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/app/icons8-dungeons-and-dragons-180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/app/icons8-dungeons-and-dragons-32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/app/icons8-dungeons-and-dragons-16.png"
+        />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/icons/app/icons8-dungeons-and-dragons.svg"
+          color="#ffffff"
+        />
+        <link
+          rel="shortcut icon"
+          href="/icons/app/icons8-dungeons-and-dragons.ico"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://dnd5e.vercel.app" />
+        <meta name="twitter:title" content="D&D5e Solid App" />
+        <meta
+          name="twitter:description"
+          content="Solid App for Dungeons & Dragons 5th edition"
+        />
+        <meta
+          name="twitter:image"
+          content="https://dnd5e.vercel.app/icons/app/icons8-dungeons-and-dragons-192.png"
+        />
+        <meta name="twitter:creator" content="@megoth" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="D&D5e Solid App" />
+        <meta
+          property="og:description"
+          content="Solid App for Dungeons & Dragons 5th edition"
+        />
+        <meta property="og:site_name" content="D&D5e Solid App" />
+        <meta property="og:url" content="https://dnd5e.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://dnd5e.vercel.app/icons/app/icons8-dungeons-and-dragons-192.png"
+        />
+      </Head>
+      <SessionProvider sessionId="dnd5e-app">
+        <AppConfig appIndexURL={APP_INDEX_URL} appVocabURL={APP_VOCAB_URL}>
+          <Component {...pageProps} />
+        </AppConfig>
+      </SessionProvider>
+    </>
   );
 }
