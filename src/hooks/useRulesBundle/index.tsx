aa8b892fa1b6810e65e7ef53b4607c8b23a7ec24
Arne Hassel <arne.hassel@gmail.com>
2024-12-21 03:07:26 +0100

    Now also filters rulesBundles

--- src/hooks/useRulesBundle/index.tsx
+++ src/hooks/useRulesBundle/index.tsx
@@ -3,22 +3,26 @@ import useApp from "../useApp";
 import { useLdo } from "@ldo/solid-react";
 import { RulesBundleShapeType } from "../../ldo/app.shapeTypes";
 
-export default function useRulesBundle(...rules: ["classes"]) {
+export default function useRulesBundle(...bundles: string[]) {
   const { getResource, getSubject } = useLdo();
   const { app } = useApp();
   return useSWR(
-    () => "rulesBundle" + app["@id"] + rules.join("-"),
+    () => "rulesBundle" + app["@id"] + bundles.join("-"),
     async () =>
       Promise.all(
         (
           await Promise.all(
-            app.rulesBundle.map(async (set) => {
-              await getResource(set["@id"]).readIfUnfetched();
-              return getSubject(RulesBundleShapeType, set["@id"]);
+            app.rulesBundle.map(async (bundle) => {
+              await getResource(bundle["@id"]).readIfUnfetched();
+              return getSubject(RulesBundleShapeType, bundle["@id"]);
             }),
           )
         )
-          .flatMap((set) => set.rulesResource)
+          .filter(
+            (bundle) =>
+              bundles.length === 0 || bundles.indexOf(bundle.label) !== -1,
+          )
+          .flatMap((bundle) => bundle.rulesResource)
           .map(async (resource) =>
             getResource(resource["@id"]).readIfUnfetched(),
           ),
