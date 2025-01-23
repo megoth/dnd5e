c896d8d7ccd564c51c4a31395b7a4f33e15e332f
Arne Hassel <arne.hassel@gmail.com>
2025-01-23 14:45:43 +0100

    Another attempt

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            // not much to save
+            // if (id.includes("@fluent")) {
+            //   return "vendor_fluent";
+            // }
+            // maybe just this failed?
+            // if (
+            //   id.includes("react") ||
+            //   id.includes("@uidotdev/usehooks") ||
+            //   id.includes("swr") ||
+            //   id.includes("clsx")
+            // ) {
+            //   return "vendor_react";
+            // }
+            if (id.includes("@ldo") || id.includes("rdf-namespaces")) {
+              return "vendor_solid";
+            }
+            return "vendor"; // all other package goes here
+          }
+        },
+      },
+    },
+  },
   plugins: [react()],
   assetsInclude: ["**/*.(md|ttl)"],
 });
